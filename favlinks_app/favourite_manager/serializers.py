3fc888458a73ec478e4264ad44f92c797820720f
cherieasa <cherieasa@gmail.com>
2024-03-29 20:44:32 +0700

    Create favouritetag, category, url serializers

---
+++ favlinks_app/favourite_manager/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from favourite_manager.models import FavouriteUrl, FavouriteCategory, FavouriteTag
+
+class FavouriteCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FavouriteCategory
+        fields = "__all__"
+
+
+class FavouriteTagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FavouriteTag
+        fields = "__all__"
+
+
+class FavouriteUrlSerializer(serializers.ModelSerializer):
+    tags = FavouriteTagSerializer(many=True)
+    category = FavouriteCategorySerializer(many=True)
+
+    class Meta:
+        model = FavouriteUrl
+        fields = [
+            "user",
+            "url",
+            "title",
+            "tags",
+            "category",
+            "created_at",
+            "updated_at",
+        ]
