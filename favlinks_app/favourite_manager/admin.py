7b8f9a065bba86c0943fe59b4f73d8a2977265e6
cherieasa <cherieasa@gmail.com>
2024-03-29 14:30:31 +0700

    Create favourite manager models, admin and migrations

--- favlinks_app/favourite_manager/admin.py
+++ favlinks_app/favourite_manager/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import FavouriteCategory, FavouriteTag, FavouriteUrl
 
-# Register your models here.
+
+class FavouriteCategoryAdminView(admin.ModelAdmin):
+    list_display = ["name", "created_at", "updated_at"]
+    search_fields = ["name"]
+    list_filter = ["created_at", "updated_at"]
+
+
+class FavouriteTagAdminView(admin.ModelAdmin):
+    list_display = ["name", "created_at", "updated_at"]
+    search_fields = ["name"]
+    list_filter = ["created_at", "updated_at"]
+
+
+class FavouriteUrlAdminView(admin.ModelAdmin):
+    list_display = ["title", "url", "created_at", "updated_at"]
+    search_fields = ["title", "url"]
+    list_filter = ["created_at", "updated_at"]
+    filter_horizontal = ["tags", "category"]  # for ManyToMany fields
+
+
+admin.site.register(FavouriteCategory, FavouriteCategoryAdminView)
+admin.site.register(FavouriteTag, FavouriteTagAdminView)
+admin.site.register(FavouriteUrl, FavouriteUrlAdminView)
--- favlinks_app/favourite_manager/models.py
+++ favlinks_app/favourite_manager/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -7,15 +8,39 @@ class FavouriteCategory(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Favourite Category")
+        verbose_name_plural = _("Favourite Categories")
+
 
 class FavouriteTag(models.Model):
     name = models.CharField(_("Tag Name"), max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Favourite Tag")
+        verbose_name_plural = _("Favourite Tags")
+
 
 class FavouriteUrl(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     url = models.URLField()
     title = models.CharField(_("Title"), max_length=255)
+    tags = models.ManyToManyField(FavouriteTag)
+    category = models.ManyToManyField(FavouriteCategory)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _("Favourite Url")
+        verbose_name_plural = _("Favourite Urls")
