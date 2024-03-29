f717ca04633bd768295d01a4438abe31957509ff
cherieasa <cherieasa@gmail.com>
2024-03-29 20:42:33 +0700

    Adjust test models for favourite url model

--- favlinks_app/favourite_manager/tests/test_models.py
+++ favlinks_app/favourite_manager/tests/test_models.py
@@ -10,31 +10,31 @@ class FavouriteUrlTestCase(BaseTestCase):
         self.other_category = self.given_a_favourite_category(user=self.other_user)
 
     def test_user_cannot_add_other_tags_on_save_favourite_url(self):
-        fav_url = self.given_a_favourite_url(self.user, url="google.com")
+        fav_url = self.given_a_favourite_url(self.user)
         with self.assertRaises(ValueError):
             fav_url.save(tags=[self.other_tag])
 
     def test_user_cannot_add_other_tags_on_add_favourite_url(self):
-        fav_url = self.given_a_favourite_url(self.user, url="google.com")
+        fav_url = self.given_a_favourite_url(self.user)
         with self.assertRaises(ValueError):
             fav_url.tags.add(self.other_tag)
 
     def test_user_cannot_add_other_tags_on_set_favourite_url(self):
-        fav_url = self.given_a_favourite_url(self.user, url="google.com")
+        fav_url = self.given_a_favourite_url(self.user)
         with self.assertRaises(ValueError):
             fav_url.tags.set([self.other_tag])
 
     def test_user_cannot_add_other_categories_on_save_favourite_url(self):
-        fav_url = self.given_a_favourite_url(self.user, url="google.com")
+        fav_url = self.given_a_favourite_url(self.user)
         with self.assertRaises(ValueError):
             fav_url.save(categories=[self.other_category])
 
     def test_user_cannot_add_other_categories_on_add_favourite_url(self):
-        fav_url = self.given_a_favourite_url(self.user, url="google.com")
+        fav_url = self.given_a_favourite_url(self.user)
         with self.assertRaises(ValueError):
             fav_url.categories.add(self.other_category)
 
     def test_user_cannot_add_other_categories_on_set_favourite_url(self):
-        fav_url = self.given_a_favourite_url(self.user, url="google.com")
+        fav_url = self.given_a_favourite_url(self.user)
         with self.assertRaises(ValueError):
             fav_url.categories.set([self.other_category])
