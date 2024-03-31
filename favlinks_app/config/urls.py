b1c2bffc58ab03db9b11f26d72031791f6455565
Cherie <terada@divertise.asia>
2024-03-31 19:25:44 +0700

    install drf_yasg and add swagger to urls.py

--- favlinks_app/config/urls.py
+++ favlinks_app/config/urls.py
@@ -17,11 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 from favourite_manager.views import (
     FavouriteCategoryViewSet,
     FavouriteTagViewSet,
     FavouriteUrlViewSet,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version="v1",
+        description="API for application",
+    ),
 )
 
 router = DefaultRouter()
@@ -35,4 +45,9 @@ router.register(r"favouriteurl", FavouriteUrlViewSet, basename="favouriteurl")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
