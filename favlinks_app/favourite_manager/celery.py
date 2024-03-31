2c88a9cc2695aa821e2715131c000a4c64dd234b
Cherie <terada@divertise.asia>
2024-03-31 19:14:35 +0700

    add /static for admin to gitignore

---
+++ favlinks_app/favourite_manager/celery.py
@@ -0,0 +1,23 @@
+from celery import Celery
+from celery.schedules import crontab
+from django.conf import settings
+
+app = Celery("favlinks-celery")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(minute=0, hour="*"),  # Every hour
+        validate_urls_and_update_titles.s(),
+    )
+
+
+@app.task(bind=True)
+def validate_urls_and_update_titles():
+    from favourite_manager.models import ValidUrl
+
+    for valid_url_obj in ValidUrl.objects.all():
+        valid_url_obj.validate_url_and_get_title()
